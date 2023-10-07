46d826e7c105477a5b05bf2c18134cc45b9a049f
NishikantaRay <nishikantaray1@gmail.com>
2023-10-07 23:18:04 +0530

    added producthunt button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Analytics from "../components/Analytics/Analytics";
 import Course from "../components/course/Course";
 import Team from "../components/Team/Team";
 import Footer from "../components/Footer/Footer";
+import ProductHunt from "../components/Buttons/ProductHunt/ProductHunt";
 
 export default function Home() {
   return (
@@ -14,21 +15,7 @@ export default function Home() {
       <Analytics />
       <Course />
       <Team />
-      <div className="product_hunt">
-        <a
-          href="https://www.producthunt.com/posts/studytub?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-studytub"
-          target="_blank"
-        >
-          <img
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=350364&theme=dark"
-            alt="StudyTub&#0032; - Your&#0032;One&#0032;Stop&#0032;for&#0032;all&#0032;the&#0032;Notes | Product Hunt"
-            style={{ width: "250px", height: "54px" }}
-            width="250"
-            height="54"
-          />
-        </a>
-      </div>
-
+      <ProductHunt />
       <Footer />
     </div>
   );
